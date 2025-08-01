40ec5c3724c21ee8f491bac9c4df51223372757d
neziscat <jbag6789@naver.com>
2025-08-01 14:40:47 +0900

    계산기

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <h1>{{ message }}</h1>
+  <Calc></Calc>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import Calc from './components/Calc.vue';
 
 const message = ref<string>("소고기")
 </script>
