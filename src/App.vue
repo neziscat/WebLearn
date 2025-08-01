e36c1e919b5f3538595d9c8dc203c204b17f2b26
neziscat <jbag6789@naver.com>
2025-08-01 14:07:02 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "소고기 world!"
+    }
+  },
+
+  // `mounted` is a lifecycle hook which we will explain later
+  mounted() {
+    console.log(this.data().message)
+  }
+}
+</script>
+
+<style scoped>
+</style>
