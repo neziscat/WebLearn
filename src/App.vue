84a5d65b8ac6dc7a688f8efb73cb11daee3ff856
neziscat <jbag6789@naver.com>
2025-08-02 13:07:09 +0900

    잔디 심은 내역 확인 하는 사이트 제작 완료

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div class="messagebox">
+    <h1>잔디 심은 내역</h1>
+    <input class="input" v-model="id"></input>
+    <button class="button" @click="setId">확인</button>
+  </div>
+  <img :src="grass" @error="onImageError"/>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const message = ref<string>("소고기")
+const id = ref<string>("");
+const grass = ref<string>("");
+
+function setId() {
+    grass.value = `https://ghchart.rshah.org/${encodeURIComponent(id.value)}`
+  
+}
+function onImageError() {
+  alert('이미지를 불러올 수 없습니다. ID가 잘못됐거나 존재하지 않습니다.')
+  grass.value = ''
+}
 </script>
